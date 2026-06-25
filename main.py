b3cdc09f8cb1628b64b0bd07ab1932ab35848fbb
elderdrak3 <ngibbs000@gmail.com>
2026-06-24 21:40:50 -0400

    Add player and circle shape classes; update main game loop

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 import pygame
 from constants import *
-
+from player import Player
 from logger import log_state
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0.0
 
     while True:
         log_state()
         for event in pygame.event.get():
-            pass
+            if event.type == pygame.QUIT:
+                return
         screen.fill("black")
+        player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        
 
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
